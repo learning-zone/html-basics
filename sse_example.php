61cd3233ee763577564d269c4b3511f758ce15d2
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-04-28 16:33:10 +0530

    Delete sse_example.php

--- sse_example.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header('Content-Type: text/event-stream');
-    header('Cache-Control: no-cache');
-
-    $time = date('r');
-    echo "data: The server time is: {$time}\n\n";
-    flush();
-?>
