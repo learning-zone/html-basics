5645927805c055e7eceeec3d5c763ca8fdf773bc
Pradeep Kumar <pradeep.vwa@gmail.com>
2019-04-28 15:35:42 +0530

    HTML5 Server Sent Events

---
+++ sse_example.php
@@ -0,0 +1,8 @@
+<?php
+    header('Content-Type: text/event-stream');
+    header('Cache-Control: no-cache');
+
+    $time = date('r');
+    echo "data: The server time is: {$time}\n\n";
+    flush();
+?>
